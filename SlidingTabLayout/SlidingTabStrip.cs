3e41f9cdd7f4cfd13f9c1108d0692f4d41554b45
JOSH\Josh <robinsonbond007j@yahoo.com>
2018-03-11 22:24:58 -0600

    Started.

---
+++ SlidingTabLayout/SlidingTabStrip.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+
+namespace SlidingTabLayout
+{
+    class SlidingTabStrip
+    {
+        private const int DEFAULT_BOTTOM_BOARDER_THICKNESS_DIPS = 2;
+        private const byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0X26;
+        private const int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
+        private int[] INDICATOR_COLORS = { 0x19A319, 0x0000FC };
+        private int[] DIVIDER_COLORS = { 0xC5C5C5 };
+
+        private const int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
+        private const float DEFAULT_DIVIDER_HEIGHT = 0.5f;
+
+        //Bottom border
+        private int mBottomBorderThickness;
+        private Paint mBottomBorderPaint;
+        private int mDefaultBottomBorderColor;
+
+        //Indicator
+        private int mSelectedIndicatorThickness;
+        private Paint mSelectedIndicatorPaint;
+
+        //Divider
+        private Paint mDividerPaint;
+        private float mDividerHeight;
+
+        //Selected position and offset
+        private int mSelectedPosition;
+        private float mSelectedOffset;
+
+        //Tab colorizer
+        private SlidingTabScrollView.TabColorizer mCustomTabColorizer;
+        private SimpleTabColorizer mDefaultTabColorizer;
+    }
+}
